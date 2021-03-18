d3e3757cbe1aa129030f55345ef82dfa1c63e184
Luuk Vermeer <l.vermeer@student.fontys.nl>
2021-03-18 15:40:31 +0100

    added lombok

--- src/main/java/com/pharma/location/models/Location.java
+++ src/main/java/com/pharma/location/models/Location.java
@@ -1,10 +1,12 @@
 package com.pharma.location.models;
 
+import lombok.Data;
+
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table
+@Data
 public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,8 +22,9 @@ public class Location {
     @Column
     private String country;
 
-    //@OneToMany(mappedBy = "location")
-    //private List<Patient> occupants;
+    public Location() {
+
+    }
 
     public Location(Long id, String street, String houseNumber, String zipCode, String city, String country) {
         this.id = id;
@@ -29,58 +32,6 @@ public class Location {
         this.houseNumber = houseNumber;
         this.zipCode = zipCode;
         this.city = city;
-        this.country = country;
-    }
-
-    public Location() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getHouseNumber() {
-        return houseNumber;
-    }
-
-    public void setHouseNumber(String houseNumber) {
-        this.houseNumber = houseNumber;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
         this.country = country;
     }
 }
