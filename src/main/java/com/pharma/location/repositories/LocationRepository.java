6e1b341e69b1441fbe5e0b52ce6c4f8e45c56167
Luuk Vermeer <l.vermeer@student.fontys.nl>
2021-03-18 15:40:55 +0100

    added annotation.

--- src/main/java/com/pharma/location/repositories/LocationRepository.java
+++ src/main/java/com/pharma/location/repositories/LocationRepository.java
@@ -1,8 +1,10 @@
 package com.pharma.location.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.xml.stream.Location;
 
+@Repository
 public interface LocationRepository  extends JpaRepository<Location, Long> {
 }
