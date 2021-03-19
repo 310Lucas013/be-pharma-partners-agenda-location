c7b5aca52325a8029004e471c106e09d971e479f
Sander Berntsen <380685@student.fontys.nl>
2021-03-19 13:33:59 +0100

    fixed some annotations

--- src/main/java/com/pharma/location/repositories/LocationRepository.java
+++ src/main/java/com/pharma/location/repositories/LocationRepository.java
@@ -1,8 +1,9 @@
 package com.pharma.location.repositories;
 
+import com.pharma.location.models.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import javax.xml.stream.Location;
-
+@Repository
 public interface LocationRepository  extends JpaRepository<Location, Long> {
 }
