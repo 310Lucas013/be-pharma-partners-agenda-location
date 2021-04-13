78f348d49e8495b3f59e5ea69e626257bc400aa4
i389746 <389746@stundent.fontys.nl>
2021-04-13 09:29:53 +0200

    update appointment

--- src/main/java/com/pharma/location/messaging/EventReceiver.java
+++ src/main/java/com/pharma/location/messaging/EventReceiver.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -26,9 +25,6 @@ public class EventReceiver {
 
     private Logger log = LoggerFactory.getLogger(EventReceiver.class);
 
-    public EventReceiver(LocationService locationService) {
-        this.locationService = locationService;
-    }
 
     @Bean
     public Jackson2JsonMessageConverter converter() {
